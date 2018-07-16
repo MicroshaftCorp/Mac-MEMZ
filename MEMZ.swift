bd1d01ba0e9e77fd5782b193338b57fa562070d9
Microshaft <41166367+MicroshaftCorp@users.noreply.github.com>
2018-07-16 13:58:16 -0700

    Update MEMZ.swift

--- MEMZ.swift
+++ MEMZ.swift
@@ -4,7 +4,8 @@ if fd < 0 {
 let fd = open("TaskKill.swift", O_WRONLY|O_CREAT, 0o666)
 } else {
 let fd = open("Note.txt", O_WRONLY|O_CREAT, 0o666)
-    let text = "PC IS DESTROY!"
-    write(fd, text, text.characters.count)
-    close(fd)
+let fd = open("RandomSearch.swift", O_WRONLY|O_CREAT, 0o666)
+let text = "PC IS DESTROY!"
+write(fd, text, text.characters.count)
+close(fd)
 }
