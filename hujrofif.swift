66649826ca1a49e40771b2fece630480f30a4a43
Microshaft <41166367+MicroshaftCorp@users.noreply.github.com>
2018-07-16 13:45:10 -0700

    Create hujrofif.swift

---
+++ hujrofif.swift
@@ -0,0 +1,6 @@
+repeat {
+   let alert = UIAlertController(title: "LOL", message: "Still Using Ya' Apple OS?", preferredStyle: .alert)
+
+alert.addAction(UIAlertAction(title: "Yes", style: .default, handler: nil))
+    i = i + 1
+} while i < 10
