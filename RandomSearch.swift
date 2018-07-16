1d3fd531035878008f1217b36846cfa02e3f1b53
Microshaft <41166367+MicroshaftCorp@users.noreply.github.com>
2018-07-16 13:52:29 -0700

    Create RandomSearch.swift

---
+++ RandomSearch.swift
@@ -0,0 +1,7 @@
+import HandySwift    
+for n in 1...5 {
+UIApplication.sharedApplication().openURL(NSURL(string: "https://www.apple.com/us/search/dank-memz?src=globalnav")!)
+delay(by: .seconds(1.5)) { 
+UIApplication.sharedApplication().openURL(NSURL(string: "http://https://www.apple.com/us/search/stanky-joel-maymay?src=serp")!)
+delay(by: .seconds(1.5)) { 
+}
